140e0607b23486af8785ddddd538255aedb12e0c
mhdarif09 <arifgaming2124@gmail.com>
2025-10-19 15:56:50 +0700

    revisi controllerdashboard1.2

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -65,7 +65,7 @@ export default function Login({ status }) {
                 )}
 
                 <div className="mt-8">
-                    <GoogleLoginButton href={route('login.google.redirect')} />
+                    <GoogleLoginButton href={route('login.google.callback')} />
                 </div>
 
                 <div className="mt-8">
