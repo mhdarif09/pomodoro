e51fb6733d2129d764083151fe0e4b6ce5096107
mhdarif09 <arifgaming2124@gmail.com>
2025-12-29 21:07:56 +0700

    Remove build artifacts and ignore dist files

--- dist/win-unpacked/resources/app.asar.unpacked/resources/app/resources/js/Layouts/EditorLayout.jsx
+++
@@ -1,7 +0,0 @@
-export default function EditorLayout({ children }) {
-    return (
-        <div className="h-screen bg-white dark:bg-slate-900">
-            {children}
-        </div>
-    );
-}
--- dist/win-unpacked/resources/app.asar.unpacked/resources/app/storage/framework/views/dc788ff0528d3fe1d5385fde1e7b0e95.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH C:\laragon\www\pomodoro\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
