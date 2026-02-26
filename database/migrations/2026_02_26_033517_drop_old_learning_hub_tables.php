c58f4785447108c8a317e329adf5e7e98378ecc7
mhdarif09 <arifgaming2124@gmail.com>
2026-02-26 12:42:45 +0800

    fix learning hub tables drop FK constraint

--- database/migrations/2026_02_26_033517_drop_old_learning_hub_tables.php
+++ database/migrations/2026_02_26_033517_drop_old_learning_hub_tables.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
+        
         Schema::dropIfExists('user_modul_progresses');
         Schema::dropIfExists('mini_modul_chapters');
         Schema::dropIfExists('mini_moduls');
         Schema::dropIfExists('mini_modul_categories');
+        
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
